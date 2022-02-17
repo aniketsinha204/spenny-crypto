08dce4a1fe8da57b25b5c17ab5dfbbcf8e0eca50
Aniket Sinha <aniket@Anikets-MacBook-Pro.local>
2022-02-17 13:11:20 +0530

    added next-seo && next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+// TODO: add post initial setup
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://coinza.in',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+};
